6646dfe071b1c01d325c9b4ddf61b74097347f71
gabriel <matosgabriel23.la@gmail.com>
2022-11-16 20:17:09 -0400

    sintaxis de python

---
+++ holagente.py
@@ -0,0 +1,16 @@
+"""   comentario  """
+
+""" el hola mundo """
+print("hola mundo :)")
+
+""" declaracion de una variable """
+nombre="pepe"
+
+"""imprimir variable """
+
+print("mi nombre es: " + nombre)
+
+"""introducir datos a la variable desde el panel """
+
+apellido=input("escribe tu apellido:")
+print("tu apellido es:" + apellido)
